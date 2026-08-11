c06574ab941bb13e345fd850e9afacba9120c0c9
Navid.N <navidnemati888@gmail.com>
2026-08-11 14:10:29 +0330

    Update ErrorHandling

--- utils/apiError.tsx
+++ utils/apiError.tsx
@@ -1,26 +1,67 @@
+// export type ValidationErrors = Record<string, string[]>;
+
+// export function parseApiError(error: unknown): {
+//   fieldErrors?: ValidationErrors;
+//   message?: string;
+// } {
+//   if (!error || typeof error !== "object") {
+//     return { message: "خطای ناشناخته" };
+//   }
+
+//   // validation error (ModelState)
+//   if ("errors" in error) {
+//     return {
+//       fieldErrors: (error as any).errors,
+//     };
+//   }
+
+//   // custom middleware error
+//   if ("message" in error) {
+//     return {
+//       message: (error as any).message,
+//     };
+//   }
+
+//   return { message: "خطای ناشناخته" };
+// }
+
+import axios from "axios";
+
 export type ValidationErrors = Record<string, string[]>;
 
 export function parseApiError(error: unknown): {
-  fieldErrors?: ValidationErrors;
-  message?: string;
+    fieldErrors?: ValidationErrors;
+    message?: string;
 } {
-  if (!error || typeof error !== "object") {
-    return { message: "خطای ناشناخته" };
-  }
+    if (axios.isAxiosError(error)) {
 
-  // validation error (ModelState)
-  if ("errors" in error) {
+        const data = error.response?.data;
+
+        if (data?.errors) {
+            return {
+                fieldErrors: data.errors,
+                message: data.message
+            };
+        }
+
+        if (data?.message) {
+            return {
+                message: data.message
+            };
+        }
+
+        return {
+            message: "خطایی در ارتباط با سرور رخ داد"
+        };
+    }
+
+    if (error instanceof Error) {
+        return {
+            message: error.message
+        };
+    }
+
     return {
-      fieldErrors: (error as any).errors,
+        message: "خطای ناشناخته"
     };
-  }
-
-  // custom middleware error
-  if ("message" in error) {
-    return {
-      message: (error as any).message,
-    };
-  }
-
-  return { message: "خطای ناشناخته" };
 }
