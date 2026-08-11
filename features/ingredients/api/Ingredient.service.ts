84b68cff95769caede1a555133745b7c18f1491f
Navid.N <navidnemati888@gmail.com>
2026-08-11 14:15:32 +0330

    Update ingredient servic

--- features/ingredients/api/Ingredient.service.ts
+++ features/ingredients/api/Ingredient.service.ts
@@ -1,109 +1,32 @@
-import { ApiError } from "@/types/api-error";
 import { ICreateIngredient, IIngredientItem, IngredientQueryParams, IUpdateIngredient } from "@/features/ingredients/types/Ingredient";
-
-const BaseUrl = "https://localhost:7232/api/Ingredient"
-
-// export async function GetAllIngredients(): Promise<IIngredientItem[]> {
-
-//     const res = await fetch(BaseUrl)
-
-//     if (!res.ok) {
-//         const errorData: ApiError = await res.json();
-//         throw errorData
-//     }
-
-//     return res.json()
-
-// }
+import api from "@/lib/api";
 
 export async function GetAllIngredients(params?: IngredientQueryParams): Promise<IIngredientItem[]> {
 
-    let url = new URL(BaseUrl);
+    const { data } = await api.get<IIngredientItem[]>("/Ingredient", {
+        params
+    })
 
-    if (params) {
-        // اضافه کردن هر پارامتر به URL
-        Object.keys(params).forEach((key) => {
-            const value = params[key as keyof typeof params];
-            if (value !== undefined && value !== null && value !== '') {
-                url.searchParams.append(key, String(value))
-            }
-        })
-    }
-
-    const res = await fetch(url.toString())
-
-    if (!res.ok) {
-        const errorData: ApiError = await res.json();
-        throw errorData
-    }
-
-    return res.json()
+    return data;
 
 }
 
 export async function CreateIngredient(dto: ICreateIngredient): Promise<IIngredientItem> {
 
-    const res = await fetch(BaseUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(dto)
-    })
+    const { data } = await api.post<IIngredientItem>("/Ingredient", dto);
 
-    if (!res.ok) {
-        const errorData: ApiError = await res.json();
-        throw errorData
-    }
-
-    return res.json()
+    return data;
 
 }
 
-//export async function CreateIngredient(dto: ICreateIngredient): Promise<IIngredientItem> {
-//     try {
-//         const res = await fetch(BaseUrl, {
-//             method: "POST",
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify(dto)
-//         })
-
-//         if (!res.ok) {
-//             const errorData: ApiError = await res.json();
-//             throw errorData;
-//         }
-
-//         return await res.json();
-//     } catch (error) {
-//         // مدیریت خطاهای شبکه یا JSON parsing
-//         console.error("Error in CreateIngredient:", error);
-//         throw error;
-//     }
-// }
-
-
 export async function UpdateIngredient(dto: IUpdateIngredient): Promise<void> {
 
-    const res = await fetch(`${BaseUrl}/${dto.id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(dto)
-    })
-
-    if (!res.ok) {
-        const errorData: ApiError = await res.json();
-        throw errorData
-    }
+    await api.put(`/Ingredient/${dto.id}`, dto);
 
 }
 
 export async function DeleteIngredient(id: string): Promise<void> {
 
-    const res = await fetch(`${BaseUrl}/${id}`, {
-        method: "DELETE"
-    })
-
-    if (!res.ok) {
-        const errorData: ApiError = await res.json();
-        throw errorData
-    }
+    await api.delete(`/Ingredient/${id}`);
 
 }
