e766bd57732fa36af6d9eae82a839b8fab73ed80
Navid.N <navidnemati888@gmail.com>
2026-09-02 14:04:56 +0330

    Add copyright to footer

--- shared/components/footer.tsx
+++ shared/components/footer.tsx
@@ -3,11 +3,14 @@ import Container from "./container";
 export default function Footer() {
     return (
         <footer className="w-full bg-linear-to-bl from-emerald-600 to-emerald-500
-         p-5 mt-12 rounded-t-xl ring-2 ring-emerald-400">
+         pb-2 pt-4 px-3 md:pt-5 md:pb-2 mt-12 rounded-t-xl ring-2 ring-emerald-400">
             <Container>
-                <div className="text-white text-shadow-md">
+                <span className="text-white text-shadow-md">
                     با ما همیشه خوشمزه زندگی کنید.
-                </div>
+                </span>
+                <p className="text-sm text-slate-100 mt-2">
+                    نوید نعمتی 2026 &copy;
+                </p>
             </Container>
         </footer>
     )
