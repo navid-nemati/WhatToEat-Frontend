446c70b005978c857a47bbd87794f1d6ec50ccc3
Navid.N <navidnemati888@gmail.com>
2026-09-14 15:11:33 +0330

    Change HeroSearch border radius

--- shared/components/HeroSearch.tsx
+++ shared/components/HeroSearch.tsx
@@ -15,7 +15,7 @@ export default function HeroSearch() {
     return (
         <div className="w-full max-w-100 md:w-100 flex flex-nowrap items-center gap-3">
             <input
-            onChange={handleSearchChange}
+                onChange={handleSearchChange}
                 type="text"
                 className="flex-1 ring ring-emerald-300 p-3 outline-none
                     right-round pr-4
@@ -25,7 +25,13 @@ export default function HeroSearch() {
 
             <Link
                 href={`/food?search=${searchInput}`}
-                className="group flex items-center gap-2 left-round bg-amber-400 px-6 py-3 text-md md:text-lg font-medium text-emerald-950 shadow-lg shadow-amber-400/30 transition-all duration-200 ring-amber-200 hover:ring-2 hover:shadow-xl hover:shadow-amber-400/40 hover:scale-105 active:scale-95 text-shadow-sm text-nowrap">
+                className="group flex items-center gap-2 left-round 
+                    bg-amber-400 px-6 py-3 text-md md:text-lg font-medium 
+                    text-emerald-950 shadow-lg shadow-amber-400/30 transition-all 
+                    duration-200 ring-amber-200 hover:ring-2 hover:shadow-xl 
+                    hover:shadow-amber-400/40 hover:scale-105 active:scale-95 
+                    text-shadow-sm text-nowrap">
+
                 <ChefHat size={20} className="transition-transform group-hover:rotate-12" />
                 جستو
                 <span className="hidden md:flex">غذا</span>
