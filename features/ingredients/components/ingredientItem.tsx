8f97b8f573c3b0c3e3f997b52866a02e591169c0
Navid.N <navidnemati888@gmail.com>
2026-09-16 16:52:18 +0330

    Add Create ingredient animation

--- features/ingredients/components/ingredientItem.tsx
+++ features/ingredients/components/ingredientItem.tsx
@@ -59,7 +59,7 @@ export default function IngredientItem({ id, name }: IngredientItemProp) {
                 onClick={() => setIsOpen(true)}
                 className="relative bg-slate-50 border border-slate-200 px-4 py-2 rounded-lg text-slate-700 
                 hover:bg-emerald-50 hover:border-emerald-200 hover:text-emerald-600 
-                transition-all duration-200 cursor-pointer flex items-center">
+                transition-all duration-200 cursor-pointer flex items-center animate-item-pop">
                 {name}
                 {isOpen && (
                     <div className="absolute px-2 py-1 bg-white rounded-md border 
